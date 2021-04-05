e0a9204ea31d185b28e7111417c4154791a5876c
Jonathan Vander Hout <jonathanvanderhout@gmail.com>
2021-04-04 21:14:46 -0400

    Far enough to use as base for new projects.

---
+++ src/lib/Panel.svelte
@@ -0,0 +1,96 @@
+<style>
+	@import "/static/css/dashboard.css";
+	
+</style>
+<script >
+    import { onMount } from 'svelte';
+    import "/static/js/feather.min.js"; 
+    onMount(async () => {
+		feather.replace();
+    });
+    let navItems = [
+        {
+            name:"home",
+            link:"/",
+            icon:"home"
+        },
+        {
+            name:"Cool Charts",
+            link:"/charts",
+            icon:"bar-chart-2"
+        }
+        
+    ]
+</script>
+
+
+	<header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+		<a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">Awesome Dashboard</a>
+		<button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+		  <span class="navbar-toggler-icon"></span>
+		</button>
+		<input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
+		<ul class="navbar-nav px-3">
+		  <li class="nav-item text-nowrap">
+			<a class="nav-link" href="#">Sign out</a>
+		  </li>
+		</ul>
+	  </header>
+	  
+	  <div class="container-fluid">
+		<div class="row">
+		  <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+			<div class="position-sticky pt-3">
+			  <ul class="nav flex-column">
+                {#each navItems as { name,link,icon }, i}
+                    <li class="nav-item">
+                        <a class="nav-link " aria-current="page" href={link}>
+                        <span data-feather={icon}></span>
+                            {name}
+                        </a>
+                    </li>
+                    
+                {/each}
+	  
+			  <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+				<span>Saved reports</span>
+				<a class="link-secondary" href="#" aria-label="Add a new report">
+				  <span data-feather="plus-circle"></span>
+				</a>
+			  </h6>
+			  <ul class="nav flex-column mb-2">
+				<li class="nav-item">
+				  <a class="nav-link" href="#">
+					<span data-feather="file-text"></span>
+					Current month
+				  </a>
+				</li>
+				<li class="nav-item">
+				  <a class="nav-link" href="#">
+					<span data-feather="file-text"></span>
+					Last quarter
+				  </a>
+				</li>
+				<li class="nav-item">
+				  <a class="nav-link" href="#">
+					<span data-feather="file-text"></span>
+					Social engagement
+				  </a>
+				</li>
+				<li class="nav-item">
+				  <a class="nav-link" href="#">
+					<span data-feather="file-text"></span>
+					Year-end sale
+				  </a>
+				</li>
+			  </ul>
+			</div>
+		  </nav>
+          <main  class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+             <slot></slot>
+	      </main>
+          
+		</div>
+	  </div>
+
+
---
+++ src/routes/charts.svelte
@@ -0,0 +1,36 @@
+<script>
+  import Panel from "$lib/Panel.svelte";
+  import "/static/js/echarts.min.js"
+</script>
+
+<Panel>
+  <p>in the panel</p>
+  <div id="main" style="width: 600px;height:400px;"></div>
+    <script type="text/javascript">
+        // based on prepared DOM, initialize echarts instance
+        var myChart = echarts.init(document.getElementById('main'));
+
+        // specify chart configuration item and data
+        var option = {
+            title: {
+                text: 'ECharts entry example'
+            },
+            tooltip: {},
+            legend: {
+                data:['Sales']
+            },
+            xAxis: {
+                data: ["shirt","cardign","chiffon shirt","pants","heels","socks"]
+            },
+            yAxis: {},
+            series: [{
+                name: 'Sales',
+                type: 'bar',
+                data: [5, 20, 36, 10, 10, 20]
+            }]
+        };
+
+        // use configuration item and data specified to show chart
+        myChart.setOption(option);
+    </script>
+</Panel>
